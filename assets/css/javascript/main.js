dfa2d8ebc185084d60035b768dd8b4681a34ce10
scher470 <120848089+nxpinhum5326@users.noreply.github.com>
2023-08-13 20:47:44 +0300

    Create main.js

---
+++ assets/css/javascript/main.js
@@ -0,0 +1,29 @@
+var themeToggle = document.getElementById("theme-toggle");
+var sunIcon = document.getElementById("sun-icon");
+var moonIcon = document.getElementById("moon-icon");
+var body = document.body;
+
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark");
+  moonIcon.style.display = "none";
+} else {
+  body.classList.add("light");
+  sunIcon.style.display = "none";
+  localStorage.setItem("theme", "light");
+}
+
+themeToggle.addEventListener("click", function() {
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+    body.classList.add("light");
+    sunIcon.style.display = "none";
+    moonIcon.style.display = "inline-block";
+    localStorage.setItem("theme", "light");
+  } else {
+    body.classList.remove("light");
+    body.classList.add("dark");
+    moonIcon.style.display = "none";
+    sunIcon.style.display = "inline-block";
+    localStorage.setItem("theme", "dark");
+  }
+});
